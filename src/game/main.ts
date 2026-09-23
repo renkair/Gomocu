f6c83ce6bf7f70af623bdd8370f4c42536af9d94
Meraki3s4d <1913328028@qq.com>
2026-09-23 12:32:54 +0200

    fix

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,7 +2,7 @@ import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
+import { AUTO, Game, Scale } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
@@ -12,8 +12,8 @@ const config: Phaser.Types.Core.GameConfig = {
     parent: 'game-container',
     backgroundColor: '#028af8',
     scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
         width: 1024,
         height: 768
     },
